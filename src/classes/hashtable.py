82b91713419db72b97fc05abce6ef8eda9dc30e2
Olga Shiryaeva <oshiryaeva@protonmail.com>
2020-05-29 07:39:34 -0500

    Fixed the search method, now correctly serches and returns object in a hash table

--- src/classes/hashtable.py
+++ src/classes/hashtable.py
@@ -1,21 +1,23 @@
 class HashTable:
-    def __init__(self, size=10):
+    def __init__(self, size=40):
         self.table = []
         for i in range(size):
             self.table.append([])
 
-    def insert(self, item):
-        bucket = hash(item) % len(self.table)
+    def insert(self, key, package):
+        bucket = hash(key) % len(self.table)
+        print('Insert Bucket', bucket)
         bucket_list = self.table[bucket]
-        bucket_list.append(item)
+        bucket_list.append(package)
 
     def search(self, key):
         bucket = hash(key) % len(self.table)
+        print('Search bucket', bucket)
         bucket_list = self.table[bucket]
+        print('Bucket list', bucket_list[0].package_id)
 
-        if key in bucket_list:
-            item_index = bucket_list.index(key)
-            return bucket_list[item_index]
+        if bucket_list[0].package_id == key:
+            return bucket_list[0]
         else:
             return None
 
