2590ba1bfbd1d0d0fc22d81e78c468dc3f9bde2a
Olga Shiryaeva <oshiryaeva@protonmail.com>
2020-06-03 20:51:08 -0500

    Clean up

--- main.py
+++ main.py
@@ -1,113 +1,87 @@
 import operator
 import csv
+import re
 
 from src.classes.vertex import Vertex
 from src.classes.graph import Graph
 from src.classes.hashtable import HashTable
 from src.classes.package import Package
+from src.classes.truck import Truck
 
 
-def dijkstra_shortest_path(g, start_vertex):
-    unvisited_queue = []
-    # print(g.adjacency_list)
-    for current_vertex in g.adjacency_list:
-        unvisited_queue.append(current_vertex)
-
-    start_vertex.distance = 0
-
-    while len(unvisited_queue) > 0:
-
-        smallest_index = 0
-        # this loop just finds the shortest distance from the given node for all the nodes in the list
-        for i in range(1, len(unvisited_queue)):
-            # if the next nodes distance to previous node is smaller than distance of the previous node
-            # then remember the index
-            # repeat for all elements in the unvisited queue, which is a list of all the nodes in g.adjacency list
-            # print(unvisited_queue[i])
-            if unvisited_queue[i].distance < unvisited_queue[smallest_index].distance:
-                smallest_index = i
-        #         remove that node with the smallest distance from array and assign to current_vertex
-        current_vertex = unvisited_queue.pop(smallest_index)
-        # adjacency_list is the list of nodes that are connected to it, for example from the output
-        # <src.classes.vertex.Vertex object at 0x10e08b670>: [<src.classes.vertex.Vertex object at 0x10e147f70>,
-        # <src.classes.vertex.Vertex object at 0x10e183820>, <src.classes.vertex.Vertex object at 0x10e1838b0>]
-
-        # these are the nodes connected [<src.classes.vertex.Vertex object at 0x10e147f70>,
-        #  <src.classes.vertex.Vertex object at 0x10e183820>, <src.classes.vertex.Vertex object at 0x10e1838b0>]
-
-        for adj_vertex in g.adjacency_list[current_vertex]:
-            # g.adjacency_list[current_vertex] is all the nodes that are connected to current_vertex
-            # edge_weight is the distance between two nodes
-            edge_weight = g.edge_weights[(current_vertex, adj_vertex)]
-            # edge_weight is a property of a graph
-            # each edge weight represents a distance for a particular pair of nodes, for example:
-            # (< src.classes.vertex.Vertex object at 0x10d62a670 >, < src.classes.vertex.Vertex object at 0x10d6e6f70 >): 8,
-            alternative_path_distance = current_vertex.distance + edge_weight
-
-            if alternative_path_distance < adj_vertex.distance:
-                adj_vertex.distance = alternative_path_distance
-                adj_vertex.pred_vertex = current_vertex
-
-
-def get_shortest_path(start_vertex, end_vertex):
-    path = ""
-    current_vertex = end_vertex
-    while current_vertex is not start_vertex:
-        path = " -> " + str(current_vertex.label) + path
-        current_vertex = current_vertex.pred_vertex
-    path = start_vertex.label + path
-    return path
+def shortest_between_two_points(g, start_vertex, end_vertex):
+    print((start_vertex, end_vertex))
+    print('--------------')
+    for edge in g.edge_weights:
+        # print((start_vertex, end_vertex))
+        print(edge)
+        if edge == (start_vertex, end_vertex):
+            return g.edge_weights[edge]
 
 
 # package_id, address, city, state, zip_code, deadline, weight, notes="", status="In Hub"
-def read_csv_file(file_name, h):
-    with open(file_name, newline='') as csvfile:
-        filereader = csv.reader(csvfile, delimiter=",")
-        for row in filereader:
-            package = Package(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
-            print(package.package_id)
+def read_packages_csv_file(file_name, h, packages_list):
+    with open(file_name, newline='') as csv_file:
+        file_reader = csv.reader(csv_file, delimiter=",")
+        for row in file_reader:
+            # print(row)
+            package = Package(int(row[0]), row[1], row[2], row[3], row[4], row[5], row[6], row[7])
+            # print(package.address, package.zip_code)
             h.insert(package.package_id, package)
+            packages_list.append(package)
+
+
+def read_locations_csv_file(file_name, g, h):
+    with open(file_name, newline='') as csv_file:
+        file_reader = csv.reader(csv_file, delimiter=",")
+        location_matrix = []
+        vertex_a = None
+        vertex_b = None
+        for row in file_reader:
+            row_address = re.sub('\(|\)', ' ', row[1])
+            row_address = row_address.replace('\n', '').lstrip().rstrip()
+            sliced_dist = row[2:]
+            sliced_dist.insert(0, row_address)
+            location_matrix.append(sliced_dist)
+            counter = 1
+        for row in location_matrix:
+            # vertex_a = Vertex(location_matrix[row][0])
+            g.add_vertex(row[0])
+
+        for row in range(0, len(location_matrix)):
+            for i in range(counter, len(location_matrix)):
+                #     vertex_b = Vertex(location_matrix[i][0])
+                # #     g.add_vertex(vertex_b)
+                g.add_undirected_edge(location_matrix[row][0], location_matrix[i][0],
+                                      float(location_matrix[row][i + 1]))
+            counter += 1
 
 
 h = HashTable()
-
-read_csv_file('package-data.csv', h)
-print(h.search(1))
 g = Graph()
-# each vertex is an address
-vertex_a = Vertex("A")
-vertex_b = Vertex("B")
-vertex_c = Vertex("C")
-vertex_d = Vertex("D")
-vertex_e = Vertex("E")
-vertex_f = Vertex("F")
-vertex_g = Vertex("G")
-g.add_vertex(vertex_a)
-g.add_vertex(vertex_b)
-g.add_vertex(vertex_c)
-g.add_vertex(vertex_d)
-g.add_vertex(vertex_e)
-g.add_vertex(vertex_f)
-g.add_vertex(vertex_g)
-# each edge is a path from one address to another and weight is the distance between them
-g.add_undirected_edge(vertex_a, vertex_b, 8)
-g.add_undirected_edge(vertex_a, vertex_c, 7)
-g.add_undirected_edge(vertex_a, vertex_d, 3)
-g.add_undirected_edge(vertex_b, vertex_e, 6)
-g.add_undirected_edge(vertex_c, vertex_d, 1)
-g.add_undirected_edge(vertex_c, vertex_e, 20)
-g.add_undirected_edge(vertex_d, vertex_f, 15)
-g.add_undirected_edge(vertex_d, vertex_g, 12)
-g.add_undirected_edge(vertex_e, vertex_f, 40)
-g.add_undirected_edge(vertex_f, vertex_g, 10)
+packages_to_deliver = []
+packages_delivered_together = [3, 6, 9, 13, 14, 16, 18, 19, 20, 25, 28, 32, 36, 38]
 
-# Run Dijkstra's algorithm first.
-dijkstra_shortest_path(g, vertex_a)
+read_packages_csv_file('package-data.csv', h, packages_to_deliver)
+read_locations_csv_file('distances-between-locations.csv', g, h)
 
-# Sort the vertices by the label for convenience; display shortest path for each vertex
-# from vertex_a.
-# for v in sorted(g.adjacency_list, key=operator.attrgetter("label")):
-#     if v.pred_vertex is None and v is not vertex_a:
-#         print("A to %s: no path exists" % v.label)
-#     else:
-#         print("A to %s: %s (total weight: %g)" % (v.label, get_shortest_path(vertex_a, v), v.distance))
+truck1 = Truck()
+truck1.start_time = "8:00 am"
+truck2 = Truck()
+
+for i in range(1, 41):
+    print(packages_to_deliver[i])
+    if packages_to_deliver[i].package_id not in packages_delivered_together and len(truck1.packages) < 16:
+        truck1.packages.append(packages_to_deliver[i])
+        del packages_to_deliver[i]
+for pkg in packages_to_deliver:
+    print(pkg.package_id)
+    if len(truck2.packages) < 16:
+        truck2.packages.append(pkg)
+        packages_to_deliver.remove(pkg)
+
+
+print(len(truck1.packages))
+print(len(truck2.packages))
+print(len(packages_to_deliver))
+
