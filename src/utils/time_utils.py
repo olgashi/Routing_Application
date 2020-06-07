bcf7e662516ef6caaf77d978f9a8c90cecb385bb
Olga Shiryaeva <oshiryaeva@protonmail.com>
2020-06-07 13:38:13 -0500

    Fixed bugs in time_utils (to display am/pm and leading zero when minuntes < 10 correctly)

--- src/utils/time_utils.py
+++ src/utils/time_utils.py
@@ -1,9 +1,11 @@
 def calculate_current_time(start_time, distance):
-    # am_pm = ''
-    start_time = start_time.split(' ')[0]
+    start_time_split = start_time.split(' ')
+    start_time = start_time_split[0]
+    am_pm = start_time_split[1]
     start_time = start_time.split(':')
     hour = int(start_time[0])
     mins = int(start_time[1])
+
     if distance < 18:
         mins = int(mins + ((distance / 18) * 60))
     else:
@@ -15,10 +17,11 @@ def calculate_current_time(start_time, distance):
         hour = int(hour + divmod(mins, 60)[0])
         mins = int(mins - 60)
     elif mins < 10:
-        mins = '0' + str(mins)
-    if hour > 11:
+        mins = int('0' + str(mins))
+
+    if hour > 11 and am_pm == "AM":
         am_pm = 'PM'
     else:
         am_pm = 'AM'
 
-    return str(hour) + ':' + str(mins) + ' ' + am_pm
+    return str(hour) + ':' + str(mins).zfill(2) + ' ' + am_pm
