1112e895d414a64b452ee4d01c3149154c00268e
Olga Shiryaeva <oshiryaeva@protonmail.com>
2020-06-07 12:50:30 -0500

    Added load_truck method

--- src/classes/truck.py
+++ src/classes/truck.py
@@ -1,9 +1,9 @@
 class Truck:
     """Constructor"""
     _distance: int
-
+    """'_packages' is a list of package ids that are loaded on the truck"""
+    """'_distance' tracks distance for all deliveries over the course of a single trip"""
     def __init__(self):
-        """'packages' is a list of package ids that are loaded on the truck"""
         self._packages = []
         self._start_time = ""
         self._distance = 0
@@ -44,9 +44,16 @@ class Truck:
         self._current_location = new_current_location
 
     """Add packages to list of packages that are on the truck, takes package_id as int """
-    def add_package(self, package_id):
+    def add_package(self, package_id: object):
         self._packages.append(package_id)
 
     """Remove all packages from the truck"""
     def remove_all_packages(self):
         self._packages = []
+
+    def add_new_distance(self, new_distance):
+        self._distance += new_distance
+
+    def load_truck(self, packages_l: list, packages_h: hash):
+        for index in range(1, len(packages_l)):
+            self.add_package(packages_h.search(packages_l[index]))
