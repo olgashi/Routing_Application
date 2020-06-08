f5cb17f9cc7e82bbd4947e4cddfefe1efe232545
Olga Shiryaeva <oshiryaeva@protonmail.com>
2020-06-08 14:46:01 -0500

    Replaced combine_packages_by_time with display_result_time_command to show output

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import os
 from daily_delivery import start_daily_delivery
+from src.utils.user_interface import user_input
+from src.utils.user_interface import display_package_details
+from src.utils.time_utils import display_result_time_command
+
 
 current_time, total_distance, packages_hash = start_daily_delivery()
 
 print("Welcome to The Western Governors University Parcel Service delivery system")
-print("All packages were delivered with a total of " + total_distance + " miles")
 print("Please use commands below to perform a specific action or search")
 
-while True:
-    command = input("""
-    id - look up details for a specific package;
-    time - display package status of all packages at a specific time (only accepts am/pm format, for example 8:25 am);
-    distance - display total distance for all trucks;
-    clear - clear screen;
-    exit - to exit;\n >>>\t""")
 
-    if command == 'exit':
+while True:
+    command = user_input()
+    if command.lower() == 'exit':
         exit()
-    elif command == 'clear':
+    elif command.lower() == 'clear':
         os.system('cls' if os.name == 'nt' else 'clear')
+    elif command.lower() == 'id':
+        package_id = input('Please enter a package ID: ')
+        display_package_details(package_id, packages_hash)
+    elif command.lower() == 'time':
+        provided_time = input('Please enter time in am/pm format, for example 8:25 AM: ')
+        display_result_time_command(provided_time.strip(), packages_hash)
+    elif command.lower() == 'distance':
+        print("All packages were delivered with a total of " + total_distance + " miles")
+    else:
+        print("Unable to recognize the command. Please try again.")
